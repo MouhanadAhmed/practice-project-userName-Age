409af331259aedc99418b9d4107c1935d9c2b2f6
MouhaNad-ux <83966010+MouhaNad-ux@users.noreply.github.com>
2021-06-15 01:17:09 +0200

    Add files via upload
    Fixed okay button after display error modal message

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -7,7 +7,7 @@ const ErrorModal = (props) => {
 
     return (
         <div>
-            <div className={classes.backdrop}/>
+            <div className={classes.backdrop} onClick={props.onConfirm}/>
 
             
         <Card className={classes.modal}>
@@ -18,7 +18,7 @@ const ErrorModal = (props) => {
                 <p>{props.message}</p>
             </div>
             <footer className={classes.actions}>
-                <Button>Okay</Button>
+                <Button onClick={props.onConfirm}>Okay</Button>
             </footer>
         </Card>
         </div>
